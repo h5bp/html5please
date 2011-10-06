16cecbef96b99e3010080c88f2be481e5014558f
Paul Irish <paul.irish@gmail.com>
2011-10-05 18:33:42 -0700

    fun search thing. type  to try it out.

--- js/script.js
+++ js/script.js
@@ -3,25 +3,40 @@
 */
 
 
-var radios = document.querySelectorAll('input[type="radio"]'), cells = document.querySelectorAll('tbody tr:not(.title)');
+var radios = document.querySelectorAll('input[type="radio"]'), 
+    cells = document.querySelectorAll('tbody tr:not(.title)');
     
 for(var i = 0, j = radios.length; i < j; i++) {
-  radios[i].onclick = (function(i) { return function(e) {
-    for(var z = 0, y = cells.length; z < y; z++) {
-      console.log(cells[z].classList, radios[i].value);
-      if(!cells[z].classList.contains(radios[i].value)) {
-        cells[z].classList.add('hide');        
-      } else {
-        cells[z].classList.remove('hide');                
-      }   
+  radios[i].onclick = (function(i) { 
+    return function(e) {
+      for(var z = 0, y = cells.length; z < y; z++) {
+        console.log(cells[z].classList, radios[i].value);
+        if(!cells[z].classList.contains(radios[i].value)) {
+          cells[z].classList.add('hide');        
+        } else {
+          cells[z].classList.remove('hide');                
+        }   
+      }
     }
-  }
-})(i);
+  })(i);
 }    
     
     
     
+document.querySelector('input[type="search"]').addEventListener('input', function(e){
 
+  if (e.target.value == '')
+    document.body.classList.remove('searching');
+  else
+    document.body.classList.add('searching');
+
+  if (e.target.value == 'test')
+    document.body.classList.add('test');
+  else
+    document.body.classList.remove('test');
+
+
+}, false);
 
 
 
