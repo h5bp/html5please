19b03d604e1aafbdae4208002b68691f00cd7382
Brian Blakely <anewpage.media@gmail.com>
2011-11-22 07:22:18 -0500

    list.js filtering
    Replaces existing script.js content

--- js/script.js
+++ js/script.js
@@ -3,41 +3,12 @@
 */
 
 
-var radios = document.querySelectorAll('input[type="radio"]'), 
-    cells = document.querySelectorAll('tbody tr:not(.title)');
-    
-for(var i = 0, j = radios.length; i < j; i++) {
-  radios[i].onclick = (function(i) { 
-    return function(e) {
-      for(var z = 0, y = cells.length; z < y; z++) {
-        console.log(cells[z].classList, radios[i].value);
-        if(!cells[z].classList.contains(radios[i].value)) {
-          cells[z].classList.add('hide');        
-        } else {
-          cells[z].classList.remove('hide');                
-        }   
-      }
-    }
-  })(i);
-}    
-    
-    
-    
-document.querySelector('input[type="search"]').addEventListener('input', function(e){
-
-  if (e.target.value == '')
-    document.body.classList.remove('searching');
-  else{
-    document.body.classList.add('searching');
-  }
-
-  if (e.target.value.length == 1)
-    window.scrollTo(0,0);
-
-  if (e.target.value == 'test')
-    document.body.classList.add('test');
-  else
-    document.body.classList.remove('test');
-
-
+var listOptions = {
+		listClass: 'features',
+		valueNames: ['feature-name', 'feature-recco']
+	},
+	featureList = new List('gfs', listOptions);
+	
+document.getElementById('livesearch').addEventListener('search', function() {
+	featureList.search(this.value, listOptions.valueNames);
 }, false);
