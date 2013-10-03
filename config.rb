b1ed152a573936bb0352c1c7e00ed04ce31e346e
Geoff Young <geoffreysyoung@gmail.com>
2013-10-03 00:02:47 -0400

    Compass sourcemap support
    Sourcemaps working with `compass watch .` by using
    the compass-sourcemap package.

--- config.rb
+++ config.rb
@@ -3,12 +3,13 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-sass_dir = "sass"
+sass_dir = "css/sass"
 images_dir = "img"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :nested
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
@@ -16,6 +17,8 @@ relative_assets = true
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
 
+# Sass options
+sass_options = {:sourcemap => true}
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
